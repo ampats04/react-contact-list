1124778cfa30b14a5a9177b7ffd40442958212bd
Jeremy Andy Ampatin <jeremyandyampatin@gmail.com>
2024-09-19 21:48:01 +0800

    chore(app): initial commit

---
+++ contact_list/src/pages/App.jsx
@@ -0,0 +1,16 @@
+import '../styles/App.css'
+import '../styles/App.css'
+import Form from './form'
+
+
+  function App() {
+    return (
+      <>
+        <Form />
+      </>
+    )
+  }
+
+  export default App
+
+
